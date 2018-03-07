650e4607e10a36a135711e95440e5bc406127c3a
Scott Olsen <scg.olsen@gmail.com>
2018-03-07 14:57:45 -0500

    Update keys tests and keys functions.

--- plunder.js
+++ plunder.js
@@ -3,3 +3,4 @@ let tarski = require('tarski');
 
 exports.core = require('./lib/core');
 exports.meta = require('./lib/meta');
+exports.keys = require('./lib/keys');
---
+++ test/keys-test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+const plunder = require('../plunder');
+const fs = require('fs');
+const example = fs.readFileSync(__dirname + "/example_data/uber.json");
+const j = JSON.parse(example);
+
+describe("Keys", function(){
+  
+  describe('#info()', function(){
+    it('Should return information about select keys in the proivded json.', function(){
+        assert.deepEqual([{key: "info", type: "object", value: j.info}], plunder.keys.info(j, "info")); 
+    }); 
+    it('Should return information about all keys in the provided json.', function(){
+      assert.deepEqual([{key: "swagger", type: "string", value: j.swagger},{key: "info", type: "object", value: j.info},{key: "host", type: "string", value: j.host},{key: "schemes", type: "object", value: j.schemes},{key: "basePath", type: "string", value: j.basePath},{key: "produces", type: "object", value: j.produces}, {key: "paths", type: "object", value: j.paths}, {key: "definitions", type: "object", value: j.definitions}], plunder.keys.info(j));  
+    })
+  });
+
+  describe('#group()', function(){
+    it('Should group keys based on predicates', function(){
+      assert.deepEqual([["info", "schemes", "produces", "paths", "definitions"],["swagger", "host", "basePath", ]], plunder.keys.group(j, x => typeof j[x] === "object", x => typeof j[x] !== "object")); 
+    });  
+  });
+
+});
