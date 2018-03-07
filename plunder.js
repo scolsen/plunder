282a6d4782c7e191ebfa9293705506d6fcbedd02
Scott Olsen <scg.olsen@gmail.com>
2018-03-07 16:54:47 -0500

    Update core, meta, and plunder.

--- plunder.js
+++ plunder.js
@@ -4,3 +4,4 @@ let tarski = require('tarski');
 exports.core = require('./lib/core');
 exports.meta = require('./lib/meta');
 exports.keys = require('./lib/keys');
+exports.api = require('./lib/api');
